e2bc463b4b6241b973850f80497c2af57222955a
Augusto Madeira Splett (Tinho) <31394293+augustosplett@users.noreply.github.com>
2023-01-27 11:25:53 -0300

    Criado docmentação no swagger para os métodos get de Livros

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 import app from "./src/app.js";
+import swaggerDocument from './swagger.json' assert { type: "json" };
+import swaggerUi from 'swagger-ui-express';
+
 const port = process.env.PORT || 3000;
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+);
 
 app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
